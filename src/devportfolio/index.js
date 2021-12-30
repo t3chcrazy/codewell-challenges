6f8bbda75e787aa66650af2da15debb8be6fe0d3
t3chcrazy <abhishekprashant09@gmail.com>
2021-12-30 23:01:58 +0530

    CSS Changes

--- src/devportfolio/index.js
+++ src/devportfolio/index.js
@@ -11,20 +11,12 @@ import Yelp from './Assets/YelpCamp.png'
 import Github from './Assets/Social Icons/Github.svg'
 import LinkedIn from './Assets/Social Icons/LinkedIn.svg'
 import Twitter from './Assets/Social Icons/Twitter.svg'
+import NavMenu from './nav'
 
 export default function Portfolio() {
     return (
         <div className = "bg-black">
-            <nav className = "container md:px-0 px-2 flex flex-row md:justify-between items-center mx-auto py-8">
-                <h3 className = "text-white text-xl font-prata">Johnathan Spectre</h3>
-                <ul className = "list-none flex md:flex-row flex-col md:relative fixed right-0 top-0 text-slate-200">
-                    <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Articles</li>
-                    <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Chats</li>
-                    <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Awards</li>
-                    <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">About</li>
-                </ul>
-                <button className = "bg-yellow-400 px-3 py-2 text-gray-900">Get in touch</button>
-            </nav>
+            <NavMenu />
             <div className = "container md:px-0 px-2 mx-auto">
                 <h1 className = "font-prata text-white md:text-6xl text-2xl leading-normal mt-10">Helping companies build<br />better,scalable software.</h1>
                 <div className = "text-stone-400 md:w-5/12 w-full leading-normal">Award-winning web developer and author, with over 15+ years of working experience with Fortune 500 companies like Apple, Google, Facebook and more.</div>
@@ -37,7 +29,7 @@ export default function Portfolio() {
                     <img className = "md:mx-0 mx-3 mb-3" src = {Verizon} alt = "Brand" />
                 </div>
                 <div className = "flex md:flex-row flex-col md:justify-between md:items-start items-center pt-12 pb-24">
-                    <button className = "md:w-[48%] flex flex-col w-full group text-left">
+                    <button className = "md:w-[48%] flex flex-col w-full group text-left md:mb-0 mb-10">
                         <img className = "w-full" src = {Spense} alt = "Spense" />
                         <div className = "font-prata text-white md:text-3xl text-xl font-bold mt-3 my-3 after:content-['\2192'] after:ml-2">Spense.com</div>
                         <div className = "text-stone-600 text-lg">
@@ -56,7 +48,7 @@ export default function Portfolio() {
             <div className = "bg-gray-300 py-12">
                 <div className = "container md:px-0 px-2 mx-auto flex md:flex-row flex-col md:justify-between items-center">
                     <div className = "md:w-[48%] w-full">
-                        <h1 className = "font-prata text-white text-4xl m-0 leading-snug mb-3">A Co-founder at one of the<br />world's largest communities</h1>
+                        <h1 className = "font-prata text-white md:text-4xl text-2xl m-0 leading-snug mb-3">A Co-founder at one of the<br />world's largest communities</h1>
                         <div className = "text-stone-600 leading-snug">
                             The combined experience I have working at the top Fortune 500 companies<br />has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.<br /><br />
                             I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to sell yourself as a developer and open them to a whole new world of opportunities.
@@ -75,8 +67,8 @@ export default function Portfolio() {
                 <div className = "text-stone-600 leading-normal md:w-1/2 w-full mt-3 mb-5">I'm active on all social media platforms listed below, but you can also send me an email and I will get back to you within 24-48 hours.</div>
                 <button className = "bg-yellow-400 px-3 py-2 text-gray-900">Get in touch</button>
             </div>
-            <footer className = "bg-gray-600 py-5 md:px-0 px-2">
-                <div className = "container mx-auto flex flex-row justify-between">
+            <footer className = "bg-gray-600 py-5">
+                <div className = "container mx-auto flex flex-row justify-between md:px-0 px-2">
                     <h3 className = "text-white text-xl font-prata">Johnathan Spectre</h3>
                     <div className = "flex flex-row items-center">
                         <img className = "ml-3" src = {Twitter} alt = "Twitter" />
---
+++ src/devportfolio/nav.js
@@ -0,0 +1,17 @@
+import Menu from './Assets/Hamburger Menu.svg'
+export default function NavMenu() {
+
+    return (
+        <nav className = "container md:px-0 px-2 flex flex-row justify-between items-center mx-auto py-8">
+            <h3 className = "text-white text-xl font-prata">Johnathan Spectre</h3>
+            <ul className = {`list-none  md:flex-row flex-col md:flex hidden right-0 top-0 text-slate-200`}>
+                <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Articles</li>
+                <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Chats</li>
+                <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Awards</li>
+                <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">About</li>
+            </ul>
+            <button className = "bg-yellow-400 px-3 py-2 md:block hidden text-gray-900">Get in touch</button>
+            <button className = "md:hidden block"><img src = {Menu} alt = "Hamburger Menu" /></button>
+        </nav>
+    )
+}

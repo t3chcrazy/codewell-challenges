a9ca25abb50c4632e1eefe49a8a51d13f43ca84d
t3chcrazy <abhishekprashant09@gmail.com>
2022-02-13 19:10:05 +0530

    Small margin change

--- src/chirplanding/components/TestimonialCard.js
+++ src/chirplanding/components/TestimonialCard.js
@@ -4,7 +4,7 @@ import Heart from '../Assets/Like.svg'
 export default function TestimonialCard({ user, hasAdditionalHeight }) {
     const { likes, createdDate, review, userName, twitterHandle, image } = user
     return (
-        <div className = "rounded-md bg-white p-4 lg:w-[32%] md:w-1/2 flex-shrink-0 w-full lg:mr-0 md:mr-3 mr-0">
+        <div className = "rounded-md bg-white p-4 lg:w-[32%] md:w-1/2 flex-shrink-0 w-full lg:mr-0 md:mr-3 mr-0 md:mb-0 mb-3">
             <div className = "flex flex-row items-center">
                 <img src = {image} alt = "Testimonial avatar" />
                 <div className = "flex-1 ml-1">
--- src/devportfolio/nav.js
+++ src/devportfolio/nav.js
@@ -1,6 +1,8 @@
 import Menu from './Assets/Hamburger Menu.svg'
 export default function NavMenu() {
 
+    const handleClick = () => window.location.replace("https://www.linkedin.com/in/abhishek-prashant-92b152a9/")
+
     return (
         <nav className = "container flex flex-row justify-between items-center mx-auto py-8">
             <h3 className = "text-white text-xl font-prata">Johnathan Spectre</h3>
@@ -10,7 +12,7 @@ export default function NavMenu() {
                 <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">Awards</li>
                 <li className = "cursor-pointer transition-opacity mx-2 hover:opacity-90">About</li>
             </ul>
-            <button className = "bg-yellow-400 px-3 py-2 md:block hidden text-gray-900">Get in touch</button>
+            <button onClick = {handleClick} className = "bg-yellow-400 px-3 py-2 md:block hidden text-gray-900">Get in touch</button>
             <button className = "md:hidden block"><img src = {Menu} alt = "Hamburger Menu" /></button>
         </nav>
     )
